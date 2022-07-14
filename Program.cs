152abea96aa951d67b7eee0160a393ef0e7c3d98
electronic333 <42278256+electronic333@users.noreply.github.com>
2022-07-14 19:58:28 +0700

    Add test logic

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace electronic333.SDaA_Coursework;
+
+internal static class Program {
+
+  static void Main (string[] args) {
+    System.Console.WriteLine("Hello, World!");
+  }
+
+}
---
+++ tests/TestForPass.cs
@@ -0,0 +1,13 @@
+namespace electronic333.SDaA_Coursework.Tests;
+
+using NUnit.Framework;
+
+[TestFixture]
+public static class Tests {
+
+  [Test]
+  public static void PassedTests () {
+    Assert.Pass();
+  }
+
+}
